af6a7e59290b4f72a86a7076218b05526b333ec3
guruprakashj1 <guruprakashj@gmail.com>
2024-03-12 16:24:48 +0530

    Update test.sol

--- test.sol
+++ test.sol
@@ -7,4 +7,8 @@ contract SimpleStorage {
     function setData(uint256 _data) external {
         data = _data;
     }
+
+    function getData() external view returns (uint256) {
+        return data;
+    }
 }
