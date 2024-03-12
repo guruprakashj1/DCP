18834da12baafc483148020000ebbf561a59da36
guruprakashj1 <guruprakashj@gmail.com>
2024-03-12 16:16:38 +0530

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-library test{
 
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
+contract SimpleStorage {
+    uint256 public data;
 
-        return c;
+    function setData(uint256 _data) external {
+        data = _data;
     }
 }
